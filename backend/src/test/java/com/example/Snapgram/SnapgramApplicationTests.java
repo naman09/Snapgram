61bfff9d61b715612472a000393518e9195b0eed
Naman Jain <naman.jain@salesforce.com>
2024-01-09 00:57:58 +0530

    initialize backend

---
+++ backend/src/test/java/com/example/Snapgram/SnapgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Snapgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
