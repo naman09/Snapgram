ef805508973953c6a37194293ed9fb82887131cc
Naman Jain <naman.jain@salesforce.com>
2024-01-15 04:10:03 +0530

    add logger AOP, remove spring-security, refactor

---
+++ backend/src/main/java/com/example/Snapgram/controllers/SmokeTestController.java
@@ -0,0 +1,13 @@
+package com.example.Snapgram.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/v1")
+public class SmokeTestController {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
